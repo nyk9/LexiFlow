ca7dc2eba7f4a5be348364ebffde27df068e03df
nyk9 <eijikagimoto@gmail.com>
2025-09-15 15:02:58 +0900

    Reuse PrismaClient instance in development

--- web-apps/frontend/auth.ts
+++ web-apps/frontend/auth.ts
@@ -4,7 +4,13 @@ import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
